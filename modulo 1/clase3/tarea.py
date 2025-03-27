9df47b05bcd33d786fd8867ad12bbf766b795acc
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2025-03-27 20:27:07 -0300

    .

---
+++ modulo 1/clase3/tarea.py
@@ -0,0 +1,7 @@
+# Ejercicio 1: Crear un bucle que imprima la tabla del 3.
+
+# Ejercicio 2: Crear una función que reciba una lista de números y devuelva el mayor número.
+
+# Ejercicio EXTRA: Crea una funcion que elimine empleados
+
+# PISTA: El ejercicio 2 y el ejercicio 3, van a utilizar metodos que aun no vieron, recomiendo buscar el metodo adecuado en W3Schools
